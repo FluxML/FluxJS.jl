c61f46ddc3ad9478c2e87a328ebdf96bb543ec3a
Mike Innes <mike.j.innes@gmail.com>
2018-03-16 15:04:02 +0000

    syntax

--- src/lib.jl
+++ src/lib.jl
@@ -7,12 +7,12 @@ matVecMul(args...) = *(args...)
 
 shape(::typeof(matVecMul), args...) = shape(*, args...)
 
-@primitive Trace (::typeof(*))(x::AbstractMatrix, y::AbstractMatrix) =
+@primitive Trace x::AbstractMatrix * y::AbstractMatrix =
   StagedArray(*, x, y)
 
 jscall(::typeof(*), a, b) = jscall(:(math.matMul), a, b)
 
-@primitive Trace (::typeof(*))(x::AbstractMatrix, y::AbstractVector) =
+@primitive Trace x::AbstractMatrix * y::AbstractVector =
   StagedArray(matVecMul, x, y)
 
 jscall(::typeof(matVecMul), a, b) = jscall(:(math.matrixTimesVector), a, b)
@@ -21,17 +21,17 @@ jscall(::typeof(matVecMul), a, b) = jscall(:(math.matrixTimesVector), a, b)
 
 concat1D(a, b) = vcat(a, b)
 
-@primitive Trace (::typeof(vcat))(a::AbstractVector, b::AbstractVector) =
+@primitive Trace vcat(a::AbstractVector, b::AbstractVector) =
   StagedArray(concat1D, a, b)
 
-@primitive Trace (::typeof(vcat))(a::AbstractMatrix, b::AbstractMatrix) =
+@primitive Trace vcat(a::AbstractMatrix, b::AbstractMatrix) =
   error("concat2D not implemented")
 
 jscall(::typeof(concat1D), a, b) = jscall(:(math.concat1D), a, b)
 
 # softmax
 
-@primitive Trace (::typeof(softmax))(x::AbstractVecOrMat) =
+@primitive Trace softmax(x::AbstractVecOrMat) =
   StagedArray(softmax, x)
 
 jscall(::typeof(softmax), x) = jscall(:(math.softmax), x)
